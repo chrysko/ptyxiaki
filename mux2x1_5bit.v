70adc99bf6c93327d4a487f13e8bd4ed346fd928
Kostas <chrysko90@yahoo.gr>
2015-04-25 12:42:51 +0300

    Fix WriteBack Bug #1

--- mux2x1_5bit.v
+++ mux2x1_5bit.v
@@ -4,7 +4,8 @@ wire case1, case2, case3, case4;
 //aluop kai rd
 assign case1 = choice==mips.ALUop && in1!=0;
 //add_imm kai rd
-assign case2 = choice==mips.ADD_IMM && in1!=0;
+assign case2 = choice==mips.ADD_IMM && in2!=0;
+
 //load kai rd
 assign case3 = choice==mips.LW && in2!=0;
 //JALop
@@ -12,7 +13,7 @@ assign case4 = choice==mips.JALop;
 
 //final assign
 assign out = case1 ? in1 : 
-             case2 ? in1: 
+             case2 ? in2: 
              case3 ? in2: 
              case4 ? 31 : 0; 
 
