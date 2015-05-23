17b684cbaea895f3529d225fa5e95296782ae704
Kostas <chrysko90@yahoo.gr>
2015-05-23 19:08:37 +0300

    Interlock minor updates

--- InterlockUnit.v
+++ InterlockUnit.v
@@ -1,9 +1,12 @@
 module InterlockUnit(input [5:0]op, input [5:0]ifop, input [4:0] ifidrs, 
     input [4:0] ifidrt, input [4:0]idexrt, input[4:0] exmemrt, output stallout);
 
+//maybe not fully implemented
+assign notstall = (ifop==mips.ADD_IMM || ifop==mips.Jop || ifop==mips.JALop);
+
 assign stallout = ((op == mips.LW) && (idexrt==ifidrs || idexrt==ifidrt)) ||
                   ((ifop == mips.BEQ)&& (idexrt ==ifidrs || idexrt ==ifidrt)) ||
-                  ((ifop == mips.BEQ)&& (exmemrt==ifidrs || exmemrt==ifidrt)) ;
+                  ((ifop == mips.BEQ)&& mips.EXMEMop==mips.LW && (exmemrt==ifidrs || exmemrt==ifidrt)) ;
 
 
 
