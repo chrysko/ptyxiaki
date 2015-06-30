31c39e285eb45b782db8c407984d8c782ec47675
Kostas <chrysko90@yahoo.gr>
2015-06-30 20:31:47 +0300

    Final code changes

--- InterlockUnit.v
+++ InterlockUnit.v
@@ -5,7 +5,7 @@ module InterlockUnit(input [5:0]op, input [5:0]ifop, input [4:0] ifidrs,
 assign notstall = (ifop==mips.ADD_IMM || ifop==mips.Jop || ifop==mips.JALop);
 
 assign stallout = ((op == mips.LW) && (idexrt==ifidrs || idexrt==ifidrt)) ||
-                  ((ifop == mips.BEQ) && mips.EXMEMop==mips.LW && (idexrt ==ifidrs || idexrt ==ifidrt)) ||
+                  //((ifop == mips.BEQ) && mips.EXMEMop==mips.LW && (idexrt ==ifidrs || idexrt ==ifidrt)) ||
                   ((ifop == mips.BEQ) && op==mips.ALUop && (mips.IDEXIR[15:11]!=0) && (mips.IDEXIR[15:11] ==ifidrs || mips.IDEXIR[15:11] ==ifidrt)) ||
                   ((ifop == mips.BEQ) && op==mips.ADD_IMM && (mips.IDEXIR[20:16]!=0) && (mips.IDEXIR[20:16] ==ifidrs || mips.IDEXIR[20:16] ==ifidrt)) ||
                   ((ifop == mips.BEQ) && mips.EXMEMop==mips.LW && (exmemrt==ifidrs || exmemrt==ifidrt)) ;
